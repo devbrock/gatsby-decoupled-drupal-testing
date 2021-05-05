64da34d8d77dd21086e950c4c47a0cdf0bd818ff
devbrock <tbrockb@gmail.com>
2021-05-05 10:24:18 -0500

    initial commit to set up fetching   data from drupal

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "gatsby-decoupled-drupal",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: `https://dev-drupal-api-testing.pantheonsite.io//`,
+        apiBase: `jsonapi`, // optional, defaults to `jsonapi`
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 };
